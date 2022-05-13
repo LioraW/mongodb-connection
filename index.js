b6492cd73953a965107aec38cd92ea0242c8ccb5
LioraW <lcwdmg@umsystem.edu>
2022-05-13 13:50:03 -0400

    first commit, posts connection up

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+
+const { corsOptionsRequests, corsSimpleRequests } = require('./middleware/cors')
+const bodyParser = require('body-parser')
+const removePoweredBy = require('./middleware/removePoweredBy')
+
+app.options('*', corsOptionsRequests)
+app.use(cors())
+app.use(corsSimpleRequests)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(removePoweredBy)
+
+console.log("hello")
+
+
+module.exports = app
---
+++ server.js
@@ -0,0 +1,23 @@
+const app = require('./index')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const port = 3000
+
+app.use(bodyParser.json())
+
+//import routes
+const postRoutes = require('./routes/posts')
+
+app.use('/posts', postRoutes)
+
+
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECTION,
+        () => console.log('connected to db') 
+)
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
